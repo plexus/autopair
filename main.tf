15a1ef3af49125942f22ec3254820730383c8901
Arne Brasseur <arne@arnebrasseur.net>
2019-08-29 14:47:29 +0200

    Add ansible host

--- main.tf
+++ main.tf
@@ -16,8 +16,45 @@ data "template_file" "userdata" {
   }
 }
 
+resource "exoscale_security_group" "autopair" {
+  name = "autopair"
+  description = "Security Group for Clojurians Log"
+}
+
+resource "exoscale_security_group_rules" "autopair" {
+  security_group_id = "${exoscale_security_group.autopair.id}"
+
+  ingress {
+    description = "Ping"
+    protocol = "ICMP"
+    icmp_type = 8
+    icmp_code = 0
+    cidr_list = [
+      "0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Ping6"
+    protocol = "ICMPv6"
+    icmp_type = 128
+    icmp_code = 0
+    cidr_list = [
+      "::/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    protocol = "TCP"
+    ports = [
+      "22"]
+    cidr_list = [
+      "0.0.0.0/0",
+      "::/0"]
+  }
+}
+
 resource "exoscale_compute" "autopair" {
-  display_name = "${local.instance_name}"
+  display_name = "autopair"
   template = "Linux Ubuntu 18.04 LTS 64-bit"
   zone = "ch-gva-2"
   size = "Large"
