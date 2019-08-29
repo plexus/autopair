866f1ffc7c10d74297ab4378c79e901cc2b8bbd3
Arne Brasseur <arne@arnebrasseur.net>
2019-08-29 11:48:47 +0200

    Add first playbooks

--- main.tf
+++ main.tf
@@ -11,16 +11,12 @@ provider "template" {
 data "template_file" "userdata" {
   template = "${file("userdata.sh.tmpl")}"
   vars = {
-    # For development / testing, use (uncomment) the lines below
-    # ansible_playbook_params = "--extra-vars \"ansible_python_interpreter=/usr/bin/python3 clojurians_app_fqdn=clojurians-log-staging.clojureverse.org use_demo_logs=true acme_sh_default_staging=true acme_sh_default_force_issue=true\""
-    # git_clone_params = "--single-branch --branch exoscale-deploy"
-    # The following can be used for production
-    ansible_playbook_params = "--extra-vars \"ansible_python_interpreter=/usr/bin/python3\" --extra-vars \"clojurians_app_fqdn=${local.instance_name}.clojureverse.org\""
-    git_clone_params = "--single-branch --branch exoscale-deploy"
+    ansible_playbook_params = "--extra-vars \"ansible_python_interpreter=/usr/bin/python3\""
+    git_clone_params = ""
   }
 }
 
-resource "exoscale_compute" "clojurians_log" {
+resource "exoscale_compute" "autopair" {
   display_name = "${local.instance_name}"
   template = "Linux Ubuntu 18.04 LTS 64-bit"
   zone = "ch-gva-2"
@@ -30,21 +26,14 @@ resource "exoscale_compute" "clojurians_log" {
   user_data = "${data.template_file.userdata.rendered}"
 
   security_groups = [
-    "${exoscale_security_group.clojurians_log.name}",
+    "${exoscale_security_group.autopair.name}",
   ]
 }
 
 output "ip_address" {
-  value = exoscale_compute.clojurians_log.ip_address
+  value = exoscale_compute.autopair.ip_address
 }
 
 output "username" {
-  value = exoscale_compute.clojurians_log.username
-}
-
-resource "cloudflare_record" "clojurians_log" {
-  domain = "clojureverse.org"
-  name   = "${local.instance_name}"
-  value  = "${exoscale_compute.clojurians_log.ip_address}"
-  type   = "A"
+  value = exoscale_compute.autopair.username
 }
